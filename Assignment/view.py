ce3de98a74f1a5ec52f947318075eac878ecc0e0
Maria Spirid <lan194@hotmail.com>
2021-12-29 02:10:29 +0300

    Working on final project

--- Assignment/view.py
+++ Assignment/view.py
@@ -17,18 +17,19 @@ def display_purchases(purchases, how_to_proceed, logger):
         sorted_list = sorted(purchases, key=lambda e: e.date)
     else:
         pass
-    print(f"Category{' ' * 32}Product{' ' * 18}Cost{' ' * 21}Date{' ' * 21}\n")
+    print(f"Category{' ' * 12}Product{' ' * 23}Cost{' ' * 16}Date{' ' * 16}\n")
     for p in sorted_list:
-        basic_length = 40
+        basic_length = 30
         values_to_alter = [str(p.category), str(p.name), str(p.price), str(p.date)]
         values_to_print = []
         for value in range(0, len(values_to_alter)):
             if len(values_to_alter[value]) < basic_length:
-                if value != 0:
-                    basic_length = 25
+                if value != 1:
+                    basic_length = 20
                     while len(values_to_alter[value]) < basic_length:
                         values_to_alter[value] += ' '
                 else:
+                    basic_length = 30
                     while len(values_to_alter[value]) < basic_length:
                         values_to_alter[value] += ' '
                 # logger.info(f"'{values_to_alter[value]}'")
